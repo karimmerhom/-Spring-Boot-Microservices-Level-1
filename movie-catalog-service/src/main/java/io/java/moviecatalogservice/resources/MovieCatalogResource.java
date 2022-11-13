7dbc99b48961d012a5e2137649dcb861bb0c995c
Ebrahim <karim.ebrahim.ext@nokia.com>
2022-11-13 14:44:56 +0200

    Course project done

---
+++ movie-catalog-service/src/main/java/io/java/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,46 @@
+package io.java.moviecatalogservice.resources;
+
+import io.java.moviecatalogservice.models.CatalogItem;
+import io.java.moviecatalogservice.models.Movie;
+import io.java.movieratingsservice.model.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        UserRating ratings = restTemplate.getForObject( "http://ratings-data-service/ratingsData/users/" + userId, UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating -> {
+          Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+            /*
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+             */
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+
+        }).collect(Collectors.toList());
+    }
+}
---
+++ movie-info-service/src/main/java/io/java/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,16 @@
+package io.java.movieinfoservice.resources;
+
+import io.java.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "Test name");
+    }
+}
